73047315e971787a022f3f7c389a4d90e84a86f5
François Steinel <francois.steinel@gmail.com>
2022-05-18 13:20:11 +0200

    feat, refactor: added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { MantineProvider } from '@mantine/core';
 import { ToastContainer } from 'react-toastify';
+import { pageView } from '@utils/analytics';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import 'flag-icon-css/css/flag-icons.min.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,8 +14,39 @@ import '@styles/main.scss';
 export default function App(props: AppProps) {
   const {Component, pageProps} = props;
 
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => pageView(url);
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
   return (
     <RecoilRoot>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+      />
+
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+                      
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
